c4fa0128d160250bf76e07f2bb38e83086b135d1
ion-malanca <ion.malanca@stud.fsa.upb.ro>
2026-06-10 16:36:22 +0300

    Errors Fixing

--- chamber/data/discover.mjs
+++ chamber/data/discover.mjs
@@ -1,49 +1,49 @@
 export const places = [
 {
-    name: "Parlament of Moldova",
-    image: "https://www.moldpres.md/uploads/0/images/large/990a848f8f3e33210195d64d3ffc574d_par_686cee08d5472.jpg",
+    name: "Parliament of Moldova",
+    image: "images/parlament.webp",
     address: "105 Stefan cel Mare Blvd, Chisinau",
     description: "The legislative center of Moldova and a symbol of national governance."
 },
 {
     name: "Cricova Wine Cellars",
-    image: "https://visit.chisinau.md/wp-content/uploads/2021/09/1-7.jpg",
+    image: "images/wine-cellars.webp",
     address: "Cricova, Chisinau Municipality",
     description: "One of the world's largest underground wine cities attracting visitors and investors."
 },
 {
     name: "Nativity Cathedral",
-    image: "https://humbo.s3.eu-north-1.amazonaws.com/place-photo/659431f5e368d78fe4c68aec/V8lAkZDFvG.jpeg",
+    image: "images/cathedral.webp",
     address: "Cathedral Park, Chisinau",
     description: "A historic landmark and one of Moldova's most recognized religious sites."
 },
 {
     name: "Valea Morilor Park",
-    image: "https://visit.chisinau.md/wp-content/uploads/2023/08/11-25-94qslh-kaskadnaya-lestnitsa.cd9f7e96.jpg",
+    image: "images/valea-morilor.webp",
     address: "Alexei Mateevici St, Chisinau",
     description: "A popular recreational destination with a scenic lake and walking areas."
 },
 {
     name: "Chisinau International Airport",
-    image: "https://visit.chisinau.md/wp-content/uploads/2024/05/fuk.jpg",
+    image: "images/airport.webp",
     address: "80/3 Dacia Blvd, Chisinau",
     description: "Moldova's primary international gateway for business and tourism."
 },
 {
     name: "Stefan cel Mare Central Park",
-    image: "https://thumbs.dreamstime.com/b/central-park-statue-stefan-cel-mare-great-chisinau-moldova-sunny-day-blue-sky-trees-flowers-96453195.jpg",
+    image: "images/stefan-cel-mare.webp",
     address: "Stefan cel Mare Blvd, Chisinau",
     description: "A historic urban park located in the heart of the capital."
 },
 {
     name: "Milestii Mici Winery",
-    image: "https://visit.chisinau.md/wp-content/uploads/2021/09/2-7-1024x678.jpg",
+    image: "images/milestii-mici.webp",
     address: "Milestii Mici, Ialoveni District",
     description: "Home to one of the largest wine collections in the world."
 },
 {
     name: "Moldova Innovation Technology Park",
-    image: "https://storage.moldova1.md/images/eded2c92-1004-4f70-b08e-6644fae7fe07.jpg",
+    image: "images/tech-park.webp",
     address: "Chisinau, Moldova",
     description: "A major hub supporting technology startups and digital businesses."
 }
